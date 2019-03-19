faa84c061f1222ddfbc6d7fe92ca6270a63e7dc2
Pasindu Vimansa Kulasooriya <pasinduvimansa96@gmail.com>
2019-03-19 15:57:56 +0530

    Nodemon module installed

--- using 3rd part modules/index.js
+++ using 3rd part modules/index.js
@@ -4,6 +4,6 @@ const _ = require('lodash');
 
 console.log(_.isString(true));
 
-var filteredArray = _.uniq(['Pasindu',1,2,3,'Vimansa','Pasindu']);
+var filteredArray = _.uniq(['Pasindu',1,2,3,'Vimansa','Vimansa']);
 
 console.log(filteredArray);
