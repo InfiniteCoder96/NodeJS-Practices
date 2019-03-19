10e40357b4fd5f1aef3b20d403d30e0342faed2e
Pasindu Vimansa Kulasooriya <pasinduvimansa96@gmail.com>
2019-03-19 13:30:35 +0530

    Using require

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 console.log('Starting app...');
 
 const fs = require('fs');
+const os = require('os')
+
+var user = os.userInfo();
 
 // Option one
-fs.appendFile('greetings.txt','Hello World',function (err){
+fs.appendFile('greetings.txt',`Hello ${user.username}!`,function (err){
   if(err){
     console.log('Unable to write to file');
   }
